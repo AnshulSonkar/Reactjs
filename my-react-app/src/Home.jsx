baaab4db2ddc4ce01f2469a7b4c53196917c1a08
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-07-07 23:37:05 +0530

    commit

--- my-react-app/src/Home.jsx
+++ my-react-app/src/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+// import React from 'react'
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
-const New = () => {
-    return (
-        <div>neWW fileeeee</div>
-    )
+// const Home = () => {
+//   return (
+//     <div>Home</div>
+//   )
+// }
+// const New = () => {
+//     return (
+//         <div>neWW fileeeee</div>
+//     )
     
-}
+// }
     
 
 
-export {New}
-export default Home;
+// export {New}
+// export default Home;
